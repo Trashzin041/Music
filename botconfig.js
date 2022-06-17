89fa065fcf88199a149a5ee3e567df61f2d34573
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-17 15:10:36 -0700

    Update botconfig.js

--- botconfig.js
+++ botconfig.js
@@ -22,7 +22,7 @@ module.exports = {
   
   Presence: {
     status: "online", // You can show online, idle, and dnd
-    name: ">play", // The message shown
+    name: "Music", // The message shown
     type: "LISTENING", // PLAYING, WATCHING, LISTENING, STREAMING
   },
 
