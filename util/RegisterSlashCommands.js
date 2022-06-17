5c11908eb3535bcf4c045bfc313b630568ba54e3
James <42681518+jmesfo0@users.noreply.github.com>
2022-06-17 15:55:35 -0700

    Update RegisterSlashCommands.js

--- util/RegisterSlashCommands.js
+++ util/RegisterSlashCommands.js
@@ -25,7 +25,7 @@ module.exports = (client, guild) => {
       //Creating variables like this, So you might understand my code :)
       let ClientAPI = client.api.applications(client.user.id);
       let GuildAPI = ClientAPI.guilds(guild);
-      let GuildName = client.get_guild(guild)
+      let GuildName = client.fetch_guild(guild)
 
       client.log(
         "[Slash Command]: [POST] Guild " + GuildName +
